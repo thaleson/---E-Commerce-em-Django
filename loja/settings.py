d46d9dd03772106682f72395d83322ba49448c45
felipe thaleson <thaleson19@hotmail.com>
2024-02-25 10:14:29 -0300

    FIX:little charges

--- loja/settings.py
+++ loja/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'loja.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'Degc-d34Af5dBe52B6Ab42DdGGAA3gCF',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '11731',
     }
 }
 
@@ -118,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
--- loja/urls.py
+++ loja/urls.py
@@ -24,3 +24,4 @@ urlpatterns = [
     path('pedido/', include('pedido.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
